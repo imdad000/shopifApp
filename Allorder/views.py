1decb4a37e13550bd1916a14eee6c1e443c35368
imdad000 <imdadali@iiitdmj.ac.in>
2019-07-04 02:21:14 +0530

    Final Models

--- Allorder/views.py
+++ Allorder/views.py
@@ -4,8 +4,6 @@ import os
 import json
 from django.http import HttpResponse
 from django.http import JsonResponse
-SHOPIFY_API_KEY = os.environ.get('ed132262e67d426893252a6a25146285')
-SHOPIFY_API_SECRET = os.environ.get('e498ddfb0638a28216e60c34b90cffef')
 from django.core import serializers
 from . models import Product,Customer
 from . form import UpdateForm
@@ -28,72 +26,5 @@ def create_product(request):
 
 		return render(request,'Allorder/all_order.html',context)
 
-		
 
 
-
-
-def customer_detail(request):
-	if request.method == 'GET':
-		headers = {"Accept": "application/json", "Content-Type": "application/json"}
-		r = requests.get("https://ed132262e67d426893252a6a25146285:e498ddfb0638a28216e60c34b90cffef@cab-store12.myshopify.com/admin/api/2019-04/orders.json")
-		data=r.json()
-
-		for i in range(0,len(data["orders"])):
-			email = data["orders"][i]["customer"]["email"]
-			customer_id = data["orders"][i]["customer"]["id"]
-			first_name = data["orders"][i]["customer"]["first_name"]
-			last_name = data["orders"][i]["customer"]["last_name"]
-			name = first_name + last_name
-			contactno = data["orders"][i]["customer"]["phone"]
-			allCustomer = Customer.objects.values('customer_id')
-			if allCustomer:
-				for a in allCustomer:
-					if str(customer_id) not in a["customer_id"]:
-						print("true")
-						Customer.objects.create(email=email,customer_id=customer_id,name=name,contactno=contactno)
-			else:
-				Customer.objects.create(email=email,customer_id=customer_id,name=name,contactno=contactno)
-		return JsonResponse(data)
-		
-
-
-def cutomer_page(request):
-	query_set=Customer.objects.all()
-	context={
-	'customer':query_set
-	}
-	return render(request,'Allorder/customer_detail.html', context)
-
-def edit_customer(request,customer_id):
-	customer_id = customer_id
-	email = request.POST['email']
-	contactno = request.POST['contactno']
-	print("id: ",customer_id)
-	abc = Customer.objects.filter(customer_id=customer_id).first()
-	print("data : ",abc.email)
-	Customer.objects.filter(customer_id=customer_id).update(email=email,contactno=contactno)
-	query_set=Customer.objects.all()
-	context={
-	'customer':query_set
-	}
-	return render(request,'Allorder/customer_detail.html', context)
-
-
-def edit_customer_page(request,customer_id):
-	customer_id = customer_id
-	form = UpdateForm(request.POST or None)
-	context={
-	'customer_id': customer_id,
-	'form':form
-	}
-	return render(request,'Allorder/updata_detail.html', context)
-
-def webhook_call(request):
-	r=requests.get("https://ed132262e67d426893252a6a25146285:e498ddfb0638a28216e60c34b90cffef@cab-store12.myshopify.com/admin/api/2019-04/webhooks.json")
-	data=r.json()
-	return JsonResponse(data)
-
-def get_order(request):
-	data = json.loads(request.data)
-	JsonResponse(data)
--- shopifApp/views.py
+++ shopifApp/views.py
@@ -1,11 +1,55 @@
 from django.contrib.auth import authenticate, login, get_user_model
 from django.http import HttpResponse
 from django.shortcuts import render,redirect
+import requests
+from Allorder.models import Product,Customer
+from Allorder .form import UpdateForm
 
-def home_page(request):
+def cutomer_page(request):
+	query_set=Customer.objects.all()
 	context={
-	 "title":"Hello Home Page",
-	 "content":"Welcome to Home Page"
+		'customer':query_set
 	}
+	from  django.db.utils import IntegrityError
+	if request.method == 'GET':
+		headers = {"Accept": "application/json", "Content-Type": "application/json"}
+		r = requests.get("https://ed132262e67d426893252a6a25146285:e498ddfb0638a28216e60c34b90cffef@cab-store12.myshopify.com/admin/api/2019-04/orders.json")
+		data=r.json()
+		try : 
+			# print("lenght : ",data["orders"][i]["customer"]["id"])
+			for i in range(0,len(data["orders"])):
+				email = data["orders"][i]["customer"]["email"]
+				customer_id = data["orders"][i]["customer"]["id"]
+				first_name = data["orders"][i]["customer"]["first_name"]
+				last_name = data["orders"][i]["customer"]["last_name"]
+				name = first_name + last_name
+				contactno = data["orders"][i]["customer"]["phone"]
+				Customer.objects.create(email=email,customer_id=customer_id,name=name,contactno=contactno)
+			return render(request,"home_page.html",context)
+		except IntegrityError :
+			return render(request,"home_page.html",context)
 	return render(request,"home_page.html",context)
 
+def edit_customer(request,customer_id):
+	customer_id = customer_id
+	email = request.POST['email']
+	contactno = request.POST['contactno']
+	print("id: ",customer_id)
+	abc = Customer.objects.filter(customer_id=customer_id).first()
+	print("data : ",abc.email)
+	Customer.objects.filter(customer_id=customer_id).update(email=email,contactno=contactno)
+	query_set=Customer.objects.all()
+	context={
+	'customer':query_set
+	}
+	return render(request,'customer_detail.html', context)
+
+
+def edit_customer_page(request,customer_id):
+	customer_id = customer_id
+	form = UpdateForm(request.POST or None)
+	context={
+	'customer_id': customer_id,
+	'form':form
+	}
+	return render(request,'updata_detail.html', context)
